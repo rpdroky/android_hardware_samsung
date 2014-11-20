b4d40eb3a938afeb0ce4788490b6913c62c26a12
Javi Ferrer <javi.f.o@gmail.com>
2014-11-21 04:05:51 +0530

    exynos4: open source libUMP
    - Deleted unneeded include ion.h
    - Correct libsecion build
    - Correct gralloc build and bring back ump_ref_drv_ion_import
    
    Change-Id: Ibce45da3ffc9593381dfa3d52fc5a4acc9914b78
    
    Conflicts:
    	exynos4/hal/libgralloc_ump/Android.mk
    
     Conflicts:
    	exynos4/hal/include/ump_ref_drv.h
    	exynos4/hal/libUMP/Android.mk

--- exynos4/hal/include/ump_ref_drv.h
+++ exynos4/hal/include/ump_ref_drv.h
@@ -31,11 +31,18 @@ extern "C" {
 
 typedef enum
 {
+<<<<<<< HEAD
 	/* This enum must match with the enum found in
 	 * <kernel_directory>/drivers/gpu/mali400/<ver>/ump/include/ump_ref_drv.h */
 	UMP_REF_DRV_CONSTRAINT_NONE = 0,
 	UMP_REF_DRV_CONSTRAINT_PHYSICALLY_LINEAR = 1,
 	UMP_REF_DRV_CONSTRAINT_USE_CACHE = 128,
+=======
+	/* This enum must match with the enum found in kernel_directory/drivers/gpu/mali400/r4p0/ump/include/ump_ref_drv.h */
+	UMP_REF_DRV_CONSTRAINT_NONE = 0,
+	UMP_REF_DRV_CONSTRAINT_PHYSICALLY_LINEAR = 1,
+	UMP_REF_DRV_CONSTRAINT_USE_CACHE = 4, /* TODO: for r4p0 it should be 4. Changed in order to test it with stock rom r3p2 */
+>>>>>>> c29132c... exynos4: open source libUMP
 } ump_alloc_constraints;
 
 /** Allocate an UMP handle containing a memory buffer.
